71ddd615dc5d299d93837d97c21cc5e29bda2a2c
Yongfeng LI <wliyongfeng@gmail.com>
2020-11-04 20:51:18 +0800

    Add bounty detail for polkadot

--- src/pages/Dot/Detail.js
+++ src/pages/Dot/Detail.js
@@ -1,3 +1,25 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router";
+import React, { useEffect, useState } from "react";
+import { dotNormalizedBountiesSelector } from "../../store/reducers/dotSlice";
+import Empty from "../common/Empty";
+import BountyDetail from "../common/BountyDetail";
+
 export default function DotDetail() {
-  return 'dot detail'
+  const [bounty, setBounty] = useState(null)
+  const bounties = useSelector(dotNormalizedBountiesSelector)
+  const { index } = useParams()
+
+  useEffect(() => {
+    const b = bounties.find(b => b.index === parseInt(index))
+    setBounty(b)
+  }, [bounties, index])
+
+  if (!bounty) {
+    return <Empty>No bounty found</Empty>
+  }
+
+  return (
+    <BountyDetail bounty={bounty} token="dot" />
+  )
 }
