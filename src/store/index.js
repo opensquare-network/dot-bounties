3ab32e85aaf7e060d3c7cbdb074e124dd05c86c0
qiyisi <qiyisi@qq.com>
2021-01-29 11:25:32 +0800

    Update polkadot version

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers'
+import { getDefaultMiddleware } from '@reduxjs/toolkit'
 
-const store = configureStore({ reducer: rootReducer })
+const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false
+})
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: customizedMiddleware
+})
 export default store
